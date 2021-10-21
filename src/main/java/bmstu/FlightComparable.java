3f7d1ff979dbd19035bb4059da836574e3151cfb
DionisSerbin <reaganusm@gmail.com>
2021-10-21 23:51:55 +0300

    Scripted auto-commit on change (2021-10-21 23:51:55) by gitwatch.sh

--- src/main/java/bmstu/FlightComparable.java
+++ src/main/java/bmstu/FlightComparable.java
@@ -3,5 +3,9 @@ package bmstu;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FlightComparable extends WritableComparator {
+
+    public FlightComparable(int airId, double delayTime, double airTime, double cancelled){
+
+    }
     
 }

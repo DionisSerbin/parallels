c5eef086ebada1a69f1f8fa28d18efe89a9f793e
DionisSerbin <reaganusm@gmail.com>
2021-10-22 12:52:58 +0300

    Scripted auto-commit on change (2021-10-22 12:52:58) by gitwatch.sh

--- src/main/java/bmstu/FlightGroupingComparator.java
+++ src/main/java/bmstu/FlightGroupingComparator.java
@@ -4,6 +4,11 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FlightGroupingComparator extends WritableComparator {
+
+    public FlightGroupingComparator(){
+        super(FlightGroupingComparator);
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return super.compare(a, b);

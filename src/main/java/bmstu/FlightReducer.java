d1c7f7add1574816da61c380edaea2e322467309
DionisSerbin <reaganusm@gmail.com>
2021-10-22 13:04:13 +0300

    Scripted auto-commit on change (2021-10-22 13:04:13) by gitwatch.sh

--- src/main/java/bmstu/FlightReducer.java
+++ src/main/java/bmstu/FlightReducer.java
@@ -9,6 +9,6 @@ public class FlightReducer extends Reducer<FlightComparable, Text, String, Text>
 
     @Override
     protected void reduce(FlightComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        for(int i = 0; i < )
+        for()
     }
 }

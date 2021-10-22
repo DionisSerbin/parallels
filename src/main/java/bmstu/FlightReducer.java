4ea859b1d7cdd4e8deecddcdd698a6c518ea2753
DionisSerbin <reaganusm@gmail.com>
2021-10-22 13:02:52 +0300

    Scripted auto-commit on change (2021-10-22 13:02:52) by gitwatch.sh

--- src/main/java/bmstu/FlightReducer.java
+++ src/main/java/bmstu/FlightReducer.java
@@ -1,10 +1,6 @@
 package bmstu;
 
 import java.io.IOException;
-import java.util.Iterator;
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 

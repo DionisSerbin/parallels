32e5ee608f121143e324b961bf5be186315e7d2e
DionisSerbin <reaganusm@gmail.com>
2021-10-22 14:25:56 +0300

    Scripted auto-commit on change (2021-10-22 14:25:56) by gitwatch.sh

--- src/main/java/bmstu/FlightMapper.java
+++ src/main/java/bmstu/FlightMapper.java
@@ -12,11 +12,11 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightComparable, T
         String line = value.toString();
         String[] column = line.split(",");
         if(key.get() > 0){
-            //DEST_AIRPORT_ID = 15 ARR_DELAY = 19 AIR_TIME = 22 IS_CANCELLED = 20
-            int airportId = Integer.parseInt(column[15]);
-            double delayTime = Double.parseDouble(column[19]);
-            double airTime = Double.parseDouble(column[22]);
-            double cancelled = Double.parseDouble(column[20]);
+            //DEST_AIRPORT_ID = 14 ARR_DELAY = 18 AIR_TIME = 21 IS_CANCELLED = 19
+            int airportId = Integer.parseInt(column[14]);
+            double delayTime = Double.parseDouble(column[18]);
+            double airTime = Double.parseDouble(column[21]);
+            double cancelled = Double.parseDouble(column[19]);
             if(cancelled == 1.00) {
                 context.write(new FlightComparable(airportId, 0.00, 0.00, true), value);
             }else if (delayTime > 0.00){

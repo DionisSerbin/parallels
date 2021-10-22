cde6c5778357d371cb333518a6fa421a9927b1af
DionisSerbin <reaganusm@gmail.com>
2021-10-22 12:46:19 +0300

    Scripted auto-commit on change (2021-10-22 12:46:19) by gitwatch.sh

--- src/main/java/bmstu/FlightGroupingComparator.java
+++ src/main/java/bmstu/FlightGroupingComparator.java
@@ -1,12 +1,7 @@
 package bmstu;
 
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-
 public class FlightGroupingComparator extends WritableComparator {
-    
+
 }

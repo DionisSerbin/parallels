f075dfab107fd764fab9b01a743993aefb3efe2d
DionisSerbin <reaganusm@gmail.com>
2021-10-22 12:39:26 +0300

    Scripted auto-commit on change (2021-10-22 12:39:26) by gitwatch.sh

--- src/main/java/bmstu/FlightPartitioner.java
+++ src/main/java/bmstu/FlightPartitioner.java
@@ -2,7 +2,7 @@ package bmstu;
 
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class FlightPartitioner extends Partitioner<> {
+public class FlightPartitioner extends Partitioner<FlightComparable, Text> {
 
     @Override
     public int getPartition(Object o, Object o2, int i) {

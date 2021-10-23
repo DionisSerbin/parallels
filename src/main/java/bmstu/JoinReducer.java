0825f1649a0e6e651e8cf8e730d78620cafca954
DionisSerbin <reaganusm@gmail.com>
2021-10-23 09:58:08 +0300

    Scripted auto-commit on change (2021-10-23 09:58:08) by gitwatch.sh

--- src/main/java/bmstu/JoinReducer.java
+++ src/main/java/bmstu/JoinReducer.java
@@ -1,6 +1,7 @@
 package bmstu;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.hadoop.io.Text;
@@ -13,5 +14,31 @@ public class JoinReducer extends Reducer<AirWritableComparable, Text, Text, Text
     protected void reduce(AirWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airName = new Text(iter.next().toString());
+        ArrayList<String> delayTime = new ArrayList<>();
+        // add delays
+        while (iter.hasNext()){
+            if (iter.next().toString().matches("\\s+.\\s+$")){
+                String added = iter.next().toString();
+                delayTime.add(added);
+            }
+        }
+        //calculate delays
+        float min = Float.MAX_VALUE;
+        float max = -1;
+        float summ = 0;
+        int i;
+        for (i = 0; i < delayTime.size(); i++){
+            float delayTimeNow = Float.parseFloat(delayTime.get(i));
+            summ += delayTimeNow;
+            if(delayTimeNow < min){
+                min = delayTimeNow;
+            }
+            if(delayTimeNow > max){
+                max = delayTimeNow;
+            }
+        }
+        context.write(airName, new Text("Average summ of delays = " + summ / i
+                + ", minimal delay = " + min + ", maximal delay" + max));
+
     }
 }

e933e878e30eb8bfdc4abf55c63077667a73a099
DionisSerbin <reaganusm@gmail.com>
2021-10-23 08:49:35 +0300

    Scripted auto-commit on change (2021-10-23 08:49:35) by gitwatch.sh

--- src/main/java/bmstu/FlightMapper.java
+++ src/main/java/bmstu/FlightMapper.java
@@ -14,23 +14,11 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
         if(key.get() > 0){
             //DEST_AIRPORT_ID = 14 ARR_DELAY = 18 AIR_TIME = 21 IS_CANCELLED = 19
             int airportId = Integer.parseInt(column[14]);
-            float delayTime;
+            float delayTime = Float.parseFloat(column[18]);
             if (!column[18].equals("")) {
-                delayTime = Float.parseFloat(column[18]);
-            } else {
-                delayTime = 0.0f;
+                context.write();
             }
-            float airTime;
-            if (!column[21].equals("")) {
-                airTime = Float.parseFloat(column[21]);
-            } else {
-                airTime = 0.0f;
-            }
-            if(column[19].equals("1.00")) {
-                context.write(new FlightWritableComparable(airportId, 0.0f, 0.0f, true), value);
-            }else if (delayTime > 0.0f){
-                context.write(new FlightWritableComparable(airportId, delayTime, airTime, false), value);
-            }
+
         }
     }
 }

a6abf1310c5f4e294be94588f707894276a1883f
DionisSerbin <reaganusm@gmail.com>
2021-10-23 13:23:28 +0300

    Scripted auto-commit on change (2021-10-23 13:23:28) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/FlightMapper.java
+++ src/main/java/ru/bmstu/iu9/FlightMapper.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirWritableComparable, Text> {
     private static final int DEST_AIRPORT_ID = 14;
-    private static final int ARR_DELAY = 17;
+    private static final int ARR_DELAY = 18;
     private static final String NULL_STR = "";
 
     @Override
@@ -17,7 +17,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirWritableComparab
             InterruptedException {
         String line = value.toString();
         String[] column = line.split(",");
-        if(column[DEST_AIRPORT_ID].equals("DEST_AIRPORT_ID")){
+        if(!column[DEST_AIRPORT_ID].equals("DEST_AIRPORT_ID")){
             //DEST_AIRPORT_ID = 14 ARR_DELAY = 18 AIR_TIME = 21 IS_CANCELLED = 19
             int airportId = Integer.parseInt(column[DEST_AIRPORT_ID]);
             float delayTime = Float.parseFloat(column[ARR_DELAY]);

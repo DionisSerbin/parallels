13f38bc4ca93d6cf30dca5c2ed520a84c5983af8
DionisSerbin <reaganusm@gmail.com>
2021-10-23 09:40:41 +0300

    Scripted auto-commit on change (2021-10-23 09:40:41) by gitwatch.sh

--- src/main/java/bmstu/JoinApp.java
+++ src/main/java/bmstu/JoinApp.java
@@ -4,27 +4,32 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
         import org.apache.hadoop.mapreduce.Job;
         import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-        import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 public class JoinApp {
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
-            System.err.println("Usage: FlightSortApp <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: JoinApp <input path> <output path>");
             System.exit(-1);
         }
 
         Job job = Job.getInstance();
         job.setJarByClass(JoinApp.class);
-        job.setJobName("Flight sort");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setJobName("Make join");
 
-        job.setMapperClass(FlightMapper.class);
-        job.setReducerClass(JoinReducer.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]),
+                TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]),
+                TextInputFormat.class, AirMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
         job.setPartitionerClass(FlightPartitioner.class);
         job.setGroupingComparatorClass(FlightGroupingComparator.class);
+        job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(AirWritableComparable.class);
         job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(String.class);

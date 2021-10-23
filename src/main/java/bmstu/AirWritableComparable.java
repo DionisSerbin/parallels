7ce9dea2dc29b216253c5d732aed9d392f87d166
DionisSerbin <reaganusm@gmail.com>
2021-10-23 09:06:49 +0300

    Scripted auto-commit on change (2021-10-23 09:06:49) by gitwatch.sh

--- src/main/java/bmstu/AirWritableComparable.java
+++ src/main/java/bmstu/AirWritableComparable.java
@@ -15,6 +15,12 @@ public class AirWritableComparable implements WritableComparable<AirWritableComp
 
     public AirWritableComparable(IntWritable intWritable, IntWritable intWritable1) {
         this.airportId = intWritable;
+        this.index = intWritable1;
+    }
+
+    public AirWritableComparable(){
+        airportId = new IntWritable(0);
+        index = new IntWritable(0);
     }
 
     @Override
@@ -29,46 +35,20 @@ public class AirWritableComparable implements WritableComparable<AirWritableComp
         index.readFields(d);
     }
 
-    @Override
-    public String toString() {
-        return ("bmstu.FlightComparable{" + "airportId=" + airportId + ", delayTime="
-                + delayTime + ", airTime=" + airTime + ", cancelled=" + cancelled) + "}";
-    }
-
-    public int delayCompare(Object o){
-        AirWritableComparable a = (AirWritableComparable) o;
-        if(this.delayTime > a.delayTime) {
-            return 1;
-        } else if(this.delayTime < a.delayTime){
-            return -1;
-        }
-        return 0;
-    }
 
     @Override
     public int hashCode() {
         return this.toString().hashCode();
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(this == obj){
-            return true;
-        } else if (obj == null || getClass() != obj.getClass()){
-            return false;
-        }
 
-        AirWritableComparable a = (AirWritableComparable) obj;
-        if(cancelled != a.cancelled
-                || Float.compare(a.airTime, delayTime) != 0
-                || Float.compare(a.delayTime, delayTime) != 0){
-            return false;
-        }
-        return airportId == a.airportId;
-    }
 
     @Override
     public int compareTo(AirWritableComparable o) {
-        return 0;
+        if(this.airportId.compareTo(o.airportId) == 0){
+            return this.index.compareTo(o.index);
+        } else {
+            return 0;
+        }
     }
 }

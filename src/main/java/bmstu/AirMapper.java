c0d872e1bbee0691a7b93be5f9e99113721d14b8
DionisSerbin <reaganusm@gmail.com>
2021-10-23 09:14:43 +0300

    Scripted auto-commit on change (2021-10-23 09:14:43) by gitwatch.sh

--- src/main/java/bmstu/AirMapper.java
+++ src/main/java/bmstu/AirMapper.java
@@ -1,4 +1,15 @@
 package bmstu;
 
-public class AirMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirMapper extends Mapper<LongWritable, Text, AirWritableComparable, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }

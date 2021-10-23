f7bc8c206090881d3a9c1ab890ced95399911131
DionisSerbin <reaganusm@gmail.com>
2021-10-23 13:06:45 +0300

    Scripted auto-commit on change (2021-10-23 13:06:45) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/JoinReducer.java
+++ src/main/java/ru/bmstu/iu9/JoinReducer.java
@@ -45,7 +45,7 @@ public class JoinReducer extends Reducer<AirWritableComparable, Text, Text, Text
 
     }
 
-    protected ArrayList<String> makeDelay(ArrayList){}
+    protected ArrayList<String> makeDelay(Iterator){}
 
 
 }
